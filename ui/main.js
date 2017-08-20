6407c572481d6f22a13d87ca94ec2388b95ce11b
sayli19 <30769286+sayli19@users.noreply.github.com>
2017-08-20 13:36:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,10 @@ submit.onClick = function()
 {
  var names = ['name1','name2','name3','name4'] ;  
     var list='';
-    for(var i=0;i<=names.length;i++){
-'<li>' +names[i]+ '</li>'  ;
+    for(var i=0;i<names.length;i++)
+    {
+list =+'<li>' +names[i]+ '</li>'  ;
 }
-var ul = document.getElementById('nameslist');
+var ul = document.getElementById('namelist');
 ul.innerHTML = 'list';
 };
