c5899de0135cb83d8b8b5bd4befd283048a263bf
sayli19 <30769286+sayli19@users.noreply.github.com>
2017-08-20 13:28:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onClick = function() 
+{
+ var names = ['name1','name2','name3','name4'] ;  
+    var list='';
+    for(var i=0;i<=names.length;i++)
+'<li>' +names[i]+ '</li>'  ;
+}
+var ul = 
